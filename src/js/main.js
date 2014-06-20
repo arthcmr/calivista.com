94872b6e0ce0df12a5f8ee00da1abbd573ef13f1
Arthur Camara <arthurcamara@gmail.com>
2014-06-20 14:09:38 +0200

    Add Calivista files and data

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,38 @@
-$(function(){
+$(function() {
+	//do jQuery stuff here
+	// $(".me").click(function () {
+ //        $.simpModal({
+ //            closeButton: true,
+ //            width: 415,
+ //            ajax: {
+ //                file: "pages/me.php"
+ //            }
+ //        });
+ //    });
 
-  //initialize parallax effect on elements with data-parallax=true
-  $.simpParallax();
+	$("a").each(function() {
+		var link = $(this).attr("href");
+		if($.type(link) === "string" && link.charAt(0) == "#" && link.length > 1) {
+			var target = $(link);
+			$(this).click(function(e) {
+				e.preventDefault();
+				go_to(target);
+			});
+		}
+	});
 
-  //initialize links with scroll animation
-  $.simpScroll();
- 
 });
+
+function go_to(destination) {
+    var body = $('body');
+    body.data("scrolling", true);
+
+    var final_dest = destination.offset().top;
+    if (final_dest > 0) final_dest += 50;
+    
+    $('html, body').animate({
+        scrollTop: final_dest
+    }, 800, 'easeInOutExpo', function () {
+        body.data("scrolling", false);
+    });
+}
